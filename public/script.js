80061bcb571e4359dff480e64a3db008acb8dc58
mrhuytran <dekutran@gmail.com>
2020-03-07 16:54:25 -0800

    a whole lotta change

--- public/script.js
+++
@@ -1,9 +0,0 @@
-function showDiv(id) {
-    if (document.getElementById(id).style.visibility.localeCompare("hidden") == 0) {
-        document.getElementById(id).style.visibility = "visible";
-        document.getElementById(id).style.height = "730px";
-    } else {
-        document.getElementById(id).style.visibility = "hidden";
-        document.getElementById(id).style.height = "0px";
-    }
-}
